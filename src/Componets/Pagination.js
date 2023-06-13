1a0e725e4e545afa3dd633ac3b4e6ed0fbb503d4
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-13 23:21:44 +0530

    Updated Pagination.js

--- src/Componets/Pagination.js
+++ src/Componets/Pagination.js
@@ -3,21 +3,27 @@ import { AppContext } from '../context/AppContext'
 
 const Pagination = () => {
 
-  const {page,handlePageChange, totalPage}= useContext(AppContext)
+  const {page,handlePageChange, totalPages}= useContext(AppContext)
   return (
-    <div>
-      <div>
-        {page>1&&
-          <button onClick={()=> handlePageChange(page-1)}>Previous</button>
+    <div className='w-full flex justify-center item-center border-2 fixed bottom-0 bg-white'>
+      <div className='flex justify-between w-11/12 max-w-[600px]'>
+      <div className='flex gap-x-2'>
+      { page>1 &&
+          <button
+          className='rounded-md border-2 px-4 py-1 mt-1'
+           onClick={()=> handlePageChange(page-1)}>Previous</button>
         }
-        {page<totalPage &&
-          <button onClick={()=>handlePageChange(page+1)} >
+        {page<totalPages &&
+          <button  className='rounded-md border-2 px-4 py-1 mt-1'
+           onClick={()=>handlePageChange(page+1)} >
             Next
-          </button>
+          </button>   
         }
+      </div>
+       
 
-        <p>
-          page {page} of {totalPage}
+        <p className=' font-bold text-sm'>
+          page {page} of {totalPages}
         </p>
       </div>
     </div>
