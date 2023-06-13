ba43b53b9c575db1ad7cbfd197dfb89d1cb2062d
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-13 22:33:09 +0530

    updated app.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,46 +1,47 @@
 import { createContext, useState } from "react";
 import { baseUrl } from "../baseUrl";
+
 //step1
 export const AppContext = createContext();
 
-export function AppContextProvider({childern}){
-
-    const [loading, setLoading]= useState(false);
+export default function AppContextProvider({children}) {
+    const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
-    const  [page, setPage]= useState(1);
-    const [totalPages, setTotalPages]= useState(null);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null);
 
     //data filling pending
 
-    async function fetchBlogPosts(){
+    async function fetchBlogPosts(page = 1) {
         setLoading(true);
-        let url= `${baseUrl}?page=${page}`;
+        let url = `${baseUrl}?page=${page}`;
+        console.log("printing the final URL");
         console.log(url);
-       try {
-        const result= await fetch(url);
-        const data= await result.json();
-        console.log(data);
-        setPage(data.page);
-        setTotalPages(data.totalPages);
-        setPosts(data.posts);
-
-       } catch (error) {
-        alert.log("Error while fetching data")
-        setPage(1);
-        setPosts([]);
-        setTotalPages(null);
-       }
-       setLoading(false);
-    }
-        
-    function handlePageChange(page){
-        setPage(page);
-         fetchBlogPosts(page)
+        try{
+            const result = await fetch(url);
+            const data = await result.json();
+            console.log(data);
+            setPage(data.page);
+            setPosts(data.posts);
+            setTotalPages(data.totalPages)
         }
+        catch(error) {
+            console.log("Error in fetching data");
+            setPage(1);
+            setPosts([]);
+            setTotalPages(null);
+        }
+        setLoading(false);
+    }
+
+    function handlePageChange(page) {
+        setPage(page);
+        fetchBlogPosts(page);
+    }
 
 
 
-    const value={
+    const value = {
         posts,
         setPosts,
         loading,
@@ -53,10 +54,8 @@ export function AppContextProvider({childern}){
         handlePageChange
     };
 
-    
-
     //step2
     return <AppContext.Provider value={value}>
-    {childern}
-    </AppContext.Provider>
+        {children}
+    </AppContext.Provider>;
 }
