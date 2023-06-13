ef6f25a60da503497f432ce89021b09765006799
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-13 18:41:21 +0530

    updated

---
+++ src/context/AppContext.js
@@ -0,0 +1,59 @@
+import { createContext, useState } from "react";
+import { baseUrl } from "../baseUrl";
+//step1
+export const AppContext = createContext();
+
+export function AppContextProvider({childern}){
+
+    const [loading, setLoading]= useState(false);
+    const [posts, setPosts] = useState([]);
+    const  [page, setPage]= useState(1);
+    const [totalPages, setTotalPages]= useState(null);
+
+    //data filling pending
+
+    async function fetchBlogPosts(){
+        setLoading(true);
+        let url= `${baseUrl}?page=${page}`;
+       try {
+        const result= await fetch(url);
+        const data= await result.json();
+        console.log(data);
+        setPage(data.page);
+        setTotalPages(data.totalPages);
+        setPosts(data.posts);
+
+       } catch (error) {
+        alert.log("Error while fetching data")
+        setPage(1);
+        setPosts([]);
+        setTotalPages(null);
+       }
+       setLoading(false);
+    }
+        
+    function handlePageChange(page){
+        setPage(page);
+         fetchBlogPosts(page)
+        }
+
+
+
+    const value={
+        posts,
+        setPosts,
+        loading,
+        setLoading,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages,
+        fetchBlogPosts,
+        handlePageChange
+    };
+
+    //step2
+    return <AppContext.Provider value={value}>
+    {childern}
+    </AppContext.Provider>
+}
