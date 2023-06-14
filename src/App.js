3f057d7883e802342e537996ffd6cbec992931e8
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-14 18:20:10 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,45 @@ import Pagination from "./Componets/Pagination";
 import { useContext, useEffect } from "react";
 import { AppContext } from "./context/AppContext";
 import './App.css';
+import { Routes, Route, useSearchParams } from "react-router-dom";
+import BlogPage from "./Pages/BlogPage";
+import TagPage from "./Pages/TagPage";
+import CategoryPage from "./Pages/CategoryPage";
+import { useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+
+
 
 export default function App() {
   const {fetchBlogPosts}= useContext(AppContext);
 
-useEffect(()=>{
-  fetchBlogPosts();
-},[])
+  const[searchParams ,setSearchParams]= useSearchParams();
+  const location = useLocation();
 
-  return <div className="w-full h-full flex flex-col justify-center items-center gap-y-1">
-        
-        <Header></Header>
-        <Blogs></Blogs>
-        <Pagination></Pagination>
-   </div>;
+useEffect(()=>{
+
+  const page =searchParams.get("page")??1;
+  if(location.pathname.includes("tags")){
+    //iska matlab tags vala page show karna hai
+    const tag = location.pathname.split("/").at(-1).replace("-"," ");
+    fetchBlogPosts(Number(page),tag);
+  }
+  else if(location.pathname.includes("categories")){
+    const category =location.pathname.split("/").at(-1).replace("-"," ");
+    fetchBlogPosts(Number(page),null,category);
+  }
+  else{
+
+    fetchBlogPosts(Number(page));
+  }
+},[location.pathname,location.search])
+
+  return (
+    <Routes>
+      <Route path='/'element={<Home/>} />
+      <Route path='/blog/:blogId'element={<BlogPage/>} />
+      <Route path='/tags/:tag'element={<TagPage/>} />
+      <Route path='/categories/:category'element={<CategoryPage/>} />
+    </Routes>
+  )
 }
